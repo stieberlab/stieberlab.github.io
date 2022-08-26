c0e18ebe61dce62d0f196902620edd24c6b6a599
stieberlab <94987691+stieberlab@users.noreply.github.com>
2022-08-25 21:08:29 -0700

    replace

--- mk-website.bash
+++ mk-website.bash
@@ -1,31 +1,19 @@
 #!/bin/bash
 
-
-function softwareCheck() {
-    for APP_NAME in $@; do
-        checkApp $APP_NAME
-    done
-}
-
 function mkPage () {
     content="$1"
     html="$2"
 
     echo "Rendering $html"
-    ./mkpage \
-        "content=$content" \
-        page.tmpl > $html
+    cat page.tmpl $content footer.tmpl > $html
 }
 
 function mkSite() {
     FOLDER=$1
 	
     echo "Generating HTML content for $FOLDER"
-    find . -type f | grep -v .git | grep -E ".txt$" | while read ITEM; do
-        HTML_FNAME=$(basename $ITEM .txt).html
-	echo $ITEM
-        echo site/$HTML_FNAME
-	mkPage  "$ITEM" "site/$HTML_FNAME"
+    find . -type f | grep -v .git | grep -E ".html$" | while read ITEM; do
+	mkPage  "$ITEM" "site/$ITEM"
     done
 }
 
